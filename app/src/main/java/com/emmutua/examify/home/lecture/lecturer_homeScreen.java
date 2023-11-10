8ad5fbea19122e84ce63e4d9e3314c930482c16c
erastus munyao kyalo <erastusmunya2022@gmail.com>
2023-11-10 08:26:28 +0300

    added lecturer my students logic

--- app/src/main/java/com/emmutua/examify/home/lecture/lecturer_homeScreen.java
+++ app/src/main/java/com/emmutua/examify/home/lecture/lecturer_homeScreen.java
@@ -2,13 +2,16 @@ package com.emmutua.examify.home.lecture;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+
 
 import com.emmutua.examify.R;
 import com.emmutua.examify.authentication.Login;
@@ -17,14 +20,18 @@ import com.google.firebase.auth.FirebaseAuth;
 public class lecturer_homeScreen extends AppCompatActivity {
     TextView goodMorningLecturerTextview;
     Button myStudentsButton,addMarksButton,lecturerLogOutButton;
+    Toolbar LecturerToolbar;
 
-
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lecturer_homescreen);
         goodMorningLecturerTextview = findViewById(R.id.Lecturer_greetingTextView);
         myStudentsButton = findViewById(R.id.Lecturer_myStudents_button);
+        LecturerToolbar = findViewById(R.id.Lecturer_toolbar);
+        setSupportActionBar(LecturerToolbar);
+        getSupportActionBar().setTitle("Lecturer Dashboard");
         addMarksButton = findViewById(R.id.Lecturer_AddMarks_button);
         lecturerLogOutButton = findViewById(R.id.Lecturer_log_out_button);
         lecturerLogOutButton.setOnClickListener(v ->{
@@ -44,6 +51,9 @@ public class lecturer_homeScreen extends AppCompatActivity {
          AlertDialog dialog = builder.create();
          dialog.show();
         });
-
+        myStudentsButton.setOnClickListener(v ->{
+            Intent intent = new Intent(lecturer_homeScreen.this, myStudents.class);
+            startActivity(intent);
+        });
     }
 }
---
+++ app/src/main/java/com/emmutua/examify/home/lecture/myStudents.java
@@ -0,0 +1,65 @@
+package com.emmutua.examify.home.lecture;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.Adapter;
+
+import com.emmutua.examify.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+
+public class myStudents extends AppCompatActivity {
+    RecyclerView recyclerView;
+    StudentsAdapter studentsAdapter;
+    List<Student> studentsList;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_students);
+        recyclerView = findViewById(R.id.lecturer_recyclerView);
+        studentsList = new ArrayList<>();
+        studentsAdapter = new StudentsAdapter(studentsList);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(studentsAdapter);
+
+        fetchAllMyStudentsFromFirebase();
+
+    }
+    void fetchAllMyStudentsFromFirebase(){
+        FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        // Fetch documents from student collection
+        firebaseFirestore.collection("students")
+                .get().addOnCompleteListener(task -> {
+                    if (task.isSuccessful()){
+                        studentsList.clear();
+                        for(QueryDocumentSnapshot studentDocument: task.getResult()){
+                            String studentID = studentDocument.getId();
+                            //Fetch the user document for the studentId from the users collection
+                            firebaseFirestore.collection("users").document(studentID)
+                                    .get().addOnCompleteListener(userTask ->{
+                                        if (userTask.isSuccessful()) {
+                                            DocumentSnapshot documentSnapshot= userTask.getResult();
+                                            if(documentSnapshot.exists() &&
+                                                    documentSnapshot.getString("uid").equals(studentID)){
+                                                String studentName = documentSnapshot.getString("fullName");
+                                                String studentRegNo = documentSnapshot.getString("regNo");
+                                            }
+                                        }
+                                    });
+                        }
+                    }
+                });
+    }
+}
