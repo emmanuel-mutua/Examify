d2c0bfb13a5e7880afe68cd7dfbf57c08f24dac7
erastus munyao kyalo <erastusmunya2022@gmail.com>
2023-11-17 15:22:46 +0300

    Added Remaining fields i Performance

---
+++ app/src/main/java/com/emmutua/examify/home/admin/StudentsPerformanceBar/RegisteredStudentsMissingMarksInAllCourses.java
@@ -0,0 +1,16 @@
+package com.emmutua.examify.home.admin.StudentsPerformanceBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.emmutua.examify.R;
+
+public class RegisteredStudentsMissingMarksInAllCourses extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registered_students_missing_marks_in_all_courses);
+    }
+}
